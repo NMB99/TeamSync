c93b06777b959a50530cc27b076b05eb184993bd
Nilay Bhaisare <nilaybhaisare1098@gmail.com>
2025-07-13 12:34:30 +0100

    Add AuthRequest and AuthResponse DTOs for login

---
+++ src/main/java/com/teamsync/teamsync/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.teamsync.teamsync.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String token_type = "Bearer";
+
+}
