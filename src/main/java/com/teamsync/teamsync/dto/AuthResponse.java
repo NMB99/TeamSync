62ab773a1062b0a665595542707ded4f172d124c
Nilay Bhaisare <nilaybhaisare1098@gmail.com>
2025-07-13 15:29:48 +0100

    Implement login auth: JWT Util, UserDetailsService, Security config, and AuthController

--- src/main/java/com/teamsync/teamsync/dto/AuthResponse.java
+++ src/main/java/com/teamsync/teamsync/dto/AuthResponse.java
@@ -1,15 +1,15 @@
 package com.teamsync.teamsync.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthResponse {
 
     private String token;
     private String token_type = "Bearer";
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
 
 }
